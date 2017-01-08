1361e7cf516e395a64866a956bf4dff64932bdc1
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-01-08 02:07:15 +0100

    Ejercicio 14
    Empezado

---
+++ JAVA/Ejercicios Navidad/Ejercicio 14/JuegosNavidenos.java
@@ -0,0 +1,111 @@
+/* Nombre del programa: JuegosNavidenos
+ * Análisis: Programa que presentará un menú donde se pueda elegir jugar al chicago, al barbudi o a los chinos.
+ * El proceso completo podrá ejecutarse más de una vez.
+ * Cada opción podrá repetirse mientras lo desee el usuario.
+ * Se deben validar todas las entradas posibles.
+ * Los subprogramas que se diseñen responderán de los casos de error si los hubiera.
+ * Entradas: opciones menu, opcion iniciar
+ * Salidas:
+ * 
+ * P.G.
+ * Inicio
+ * 	LeerValidarIniciar
+ * 	Mientras QuieraIniciar
+ * 		PrintMenuLeerValidarOpcion
+ * 		si (opcion no es 0)
+ * 			segun (opcion)
+ * 				caso 1: Chicago
+ * 				caso 2: Barbudi
+ * 				caso 3: Chinos
+ * 			fin_segun
+ * 		fin_si
+ * 		LeerValidarReiniciar
+ * 	fin_mientras
+ * Fin
+ * 
+ * P.G. nivel 1
+ * 
+ * Caso1: Chicago
+ * 
+ * Inicio
+ * 	TirarDados*
+ * 	SeleccionarGanador*
+ * 	LeerValidarApuesta
+ * 	JugarChicago*
+ * Fin
+ */
+ 
+//Inicio
+import java.util.*;
+import java.io.*;
+
+public class JuegosNavidenos
+{
+	public static void main (String[]args)
+	{
+		char iniciar = ' ';
+		int opcion = 0;
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		//LeerValidarIniciar
+		do
+		{
+			System.out.println("Desea ejecutar el programa? Y/N");
+			iniciar = Character.toLowerCase (teclado.next().charAt(0));
+		}
+		while (iniciar != 'y' && iniciar != 'n');
+		//Fin LeerValidarIniciar
+		
+		while (iniciar == 'y')
+		{
+			//PrintMenuLeerValidarOpcion
+			do
+			{
+				System.out.println("-----------------Menu-----------------");
+				System.out.println("----------1. Juego 'Chicago'----------");
+				System.out.println("----------2. Juego 'Barbudi'----------");
+				System.out.println("----------3. Juego 'Chinos'-----------");
+				System.out.println("----------0. Salir del menu-----------");
+				System.out.println("--------------------------------------");
+				opcion = teclado.nextInt();
+			}
+			while (opcion < 0 || opcion > 3);
+			//Fin PrintMenuLeerValidarOpcion
+			
+			if (opcion != 0)
+			{
+				switch (opcion)
+				{
+				
+					//caso 1: Chicago
+					case 1:
+					break;
+					
+					//caso 2: Barbudi
+					case 2:
+					System.out.println("[EN CONSTRUCCION]");
+					break;
+					
+					//caso 3: Chinos
+					case 3:
+					System.out.println("[EN CONSTRUCCION]");
+					break;
+				
+				}//fin_segun
+			
+			}//fin_si
+			
+			//LeerValidarReiniciar
+			do
+			{
+				System.out.println("Desea volver a ejecutar el programa? Y/N");
+				iniciar = Character.toLowerCase (teclado.next().charAt(0));
+			}
+			while (iniciar != 'y' && iniciar != 'n');
+			//Fin LeerValidarReiniciar
+			
+		}//fin_mientras
+	}//fin_main
+}//fin_pp
+//Fin
---
+++ JAVA/Ejercicios Navidad/Ejercicio 14/MetodosJuegosNavidenos.java
@@ -0,0 +1,13 @@
+public class MetodosJuegosNavidenos
+{
+	/* Prototipo:
+	 * Breve comentario: Juego 'Chicago'.
+	 * Precondiciones: Ninguna
+	 * Entradas: un entero y un real
+	 * Salidas:
+	 * Entradas/Salidas:
+	 * Postcondiciones:
+	 * 
+	 * Resguardo:
+	 */
+}//fin_pp
