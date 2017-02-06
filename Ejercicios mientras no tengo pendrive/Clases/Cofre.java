3fddcdfc7f135ee02ce19cb85ed37fdd10f9500a
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-02-06 13:08:48 +0100

    Ejercicios mientras no tengo pendrive

---
+++ Ejercicios mientras no tengo pendrive/Clases/Cofre.java
@@ -0,0 +1,16 @@
+/* Propiedades.
+ * 		Básicas:
+ * 		Derivadas:
+ * 		Compartidas:
+ * 
+ * Fórmulas:
+ * 
+ * Getters y setters:
+ * 
+ * Restricciones:
+ * Metodos añadidos:
+ */
+public class Cofre
+{
+	
+}//fin_cofre
