71af1ce67638789ce9f50f5b76a36c839b36c17e
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-05-23 12:53:50 +0200

    Main juego

--- JAVA/Proyecto fin de curso (eclipse)/src/Main/MainJuego.java
+++ JAVA/Proyecto fin de curso (eclipse)/src/Main/MainJuego.java
@@ -1,6 +1,31 @@
 package Main;
+/* Nombre del programa: MainJuego
+ * Breve comentario: Juego roge like y con combate por turnos
+ * Entradas:
+ * Salidas:
+ * Restricciones: Ninguna
+ * 
+ * PG.
+ * Inicio
+ * 	LeerValidarJugar
+ * 	Mientras QuieraJugar
+ *		EligePartida *
+ *		EligePersonaje *
+ *		EjecutarJuego
+ *		Preguntar JugarOtraVez
+ * 	Fin_Mientras QuieraJugar        
+ * Fin
+ * 
+ * Modulo EjecutarJuego Nivel 1
+ * Inicio
+ * 	
+ * Fin
+ */
 
 public class MainJuego 
 {
-
+	public static void main (String [] args)
+	{
+		
+	}
 }
