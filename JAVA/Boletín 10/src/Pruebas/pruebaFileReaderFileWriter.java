2c9eab3d7ebafa31f8abe244531d33cdd14f5880
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-05-23 12:18:57 +0200

    Pruebas fileReader

--- JAVA/Boletín 10/src/Pruebas/pruebaFileReaderFileWriter.java
+++ JAVA/Boletín 10/src/Pruebas/pruebaFileReaderFileWriter.java
@@ -1,6 +1,7 @@
 package Pruebas;
 
 import java.io.*;
+import java.util.*;
 
 public class pruebaFileReaderFileWriter 
 {
@@ -11,17 +12,21 @@ public class pruebaFileReaderFileWriter
 			String texto = null;
 			FileWriter fw = new FileWriter ("pepejava2.txt", true);
 			BufferedWriter bw = new BufferedWriter (fw);
+			Scanner entrada = new Scanner (new File ("pepejava2.txt"));
 			bw.newLine();
 			bw.write("Esto es una prueb");
 			bw.write(97);
 			bw.newLine();
-			bw.write("Esto es una prueba 2");
+			bw.write("Esto es una prueba 4");
 			bw.close();
 			
 			FileReader fr = new FileReader ("pepejava2.txt");
 			BufferedReader br = new BufferedReader (fr);
 			texto = br.readLine();
 			
+			//while ((Integer) Integer.parseInt(texto) instanceof Integer 
+			//		&& Integer.parseInt(texto) != -1)
+			//while (entrada.hasNextLine())
 			while (texto != null)
 			{
 				System.out.println(texto);
@@ -29,6 +34,16 @@ public class pruebaFileReaderFileWriter
 			}
 			
 			br.close();
+		}
+		
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		catch (EOFException e)
+		{
+			e.printStackTrace();
 		}
 		catch (IOException error)
 		{
