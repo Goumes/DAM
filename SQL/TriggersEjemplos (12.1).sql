639bb7337ee22928aad8d28e02dd1693a2ba480e
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-05-29 14:31:45 +0200

    SQL Triggers

--- SQL/TriggersEjemplos (12.1).sql
+++ SQL/TriggersEjemplos (12.1).sql
@@ -182,7 +182,13 @@ GO
 CREATE TRIGGER palabrasRepetidas ON Palabras
 AFTER INSERT AS
 BEGIN
-	
+	IF EXISTS(SELECT Palabra 
+				FROM inserted
+				WHERE Palabra IN (SELECT Palabra
+									FROM Palabras))
+	BEGIN
+		ROLLBACK
+	END
 END
 
 GO
@@ -191,9 +197,84 @@ GO
 6.- Comprueba que un pasajero no pueda entrar o salir por la misma estación más de tres
 veces el mismo día
 */
-/*7.- Haz un trigger que al insertar un viaje compruebe que no hay otro viaje simultáneo */
+
+USE LeoMetroV2
+
+GO
+
+CREATE TRIGGER pasajeroReincidente ON LM_Viajes
+AFTER INSERT AS
+BEGIN
+	IF EXISTS (SELECT IDTarjeta
+					FROM inserted
+					WHERE DAY (MomentoEntrada) = DAY (CAST (GETDATE () AS SMALLDATETIME))
+					GROUP BY IDTarjeta
+					HAVING COUNT (IDEstacionEntrada) > 3 OR COUNT (IDEstacionSalida) > 3)
+	BEGIN
+		ROLLBACK
+	END
+END
+
+GO
+
+/*7.- Haz un trigger que al insertar un viaje compruebe que no hay otro viaje simultáneo */CREATE TRIGGER viajeSimultaneo ON LM_ViajesAFTER INSERT ASBEGIN	IF EXISTS (SELECT I.ID				FROM inserted AS I				INNER JOIN				LM_Viajes AS V				ON V.ID = I.ID				WHERE I.MomentoEntrada = V.MomentoEntrada)	BEGIN		ROLLBACK	ENDENDGO
 --EJEMPLOS
 /*
 8.- Queremos evitar que se introduzcan palabras que terminen en "azo”
 */
+
+USE EJEMPLOS --VOLVER A MIRAR ESTE EJERCICIO
+
+GO
+
+CREATE TRIGGER restriccionazo ON Palabras
+AFTER INSERT AS
+BEGIN
+DECLARE @PalabraInsertada VARCHAR (30)
+	DECLARE cursorInserted CURSOR FOR SELECT Palabra FROM inserted 
+	OPEN cursorInserted
+	FETCH NEXT FROM cursorInserted INTO @PalabraInsertada
+
+	IF EXISTS (SELECT *
+					FROM inserted)
+	BEGIN
+		WHILE (@@FETCH_STATUS = 0)
+		BEGIN 
+
+			IF EXISTS (SELECT @PalabraInsertada
+						FROM Palabras
+						WHERE RIGHT(@PalabraInsertada,3) = 'azo')
+			BEGIN
+				ROLLBACK
+			END
+
+			FETCH NEXT FROM cursorInserted INTO @PalabraInsertada
+		END
+
+		CLOSE cursorInserted
+		DEALLOCATE cursorInserted
+	END
+END
+
+GO
 --Se incluye la posibilidad de que se modifiquen varias filas y de que haya que consultar otras tablas.
+
+-- LEOFEST
+USE LEOFEST
+
+GO
+
+/*9.- Cuando se inserte una nueva actuación de una banda hemos de comprobar que la banda
+no se ha disuelto en esa fecha. */
+
+CREATE TRIGGER bandaDisuelta ON LFEdiciones
+AFTER INSERT AS
+BEGIN
+	SELECT *
+		FROM inserted
+END
+
+GO
+
+/* 10 .- Comprueba mediante un trigger que en una edición no actúan más de tres bandas de la
+misma categoría. */
