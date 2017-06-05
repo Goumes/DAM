b6904dae63d57c49f0198282adeacf583f7da9b0
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-06-05 14:28:23 +0200

    Terminando la badat de la Quiniela

--- SQL/QuinielaBadat.sql
+++ SQL/QuinielaBadat.sql
@@ -31,6 +31,7 @@ CREATE TABLE Jornadas
 CREATE TABLE Sorteos
 (
 	ID INT NOT NULL,
+	Estado BIT NOT NULL,
 
 	CONSTRAINT PK_Sorteo PRIMARY KEY (ID)
 )
@@ -41,6 +42,7 @@ CREATE TABLE Boletos
 	ID INT NOT NULL,
 	IDJornada INT NOT NULL,
 	IDSorteo INT NOT NULL,
+	NumeroApuestas INT NOT NULL,
 
 	CONSTRAINT PK_Boletos PRIMARY KEY (ID), -- Pensar más atributos
 	CONSTRAINT FK_Boletos_Jornadas FOREIGN KEY (IDJornada) REFERENCES Jornadas (ID) ON UPDATE CASCADE ON DELETE NO ACTION,
@@ -72,6 +74,7 @@ CREATE TABLE Apuestas
 	ID INT NOT NULL,
 	IDBoleto INT NOT NULL,
 	Valor DECIMAL (6,2) NOT NULL,
+	Tipo VARCHAR (7) NOT NULL,
 	Prediccion1 VARCHAR (3) NOT NULL,
 	Prediccion2 VARCHAR (3) NOT NULL,
 	Prediccion3 VARCHAR (3) NOT NULL,
@@ -86,7 +89,7 @@ CREATE TABLE Apuestas
 	Prediccion12 VARCHAR (3) NOT NULL,
 	Prediccion13 VARCHAR (3) NOT NULL,
 	Prediccion14 VARCHAR (3) NOT NULL,
-	PrediccionPleno VARCHAR (4) NULL,
+	PrediccionPleno VARCHAR (4) NOT NULL,
 
 	CONSTRAINT PK_Apuesta PRIMARY KEY (ID, IDBoleto),
 	CONSTRAINT FK_Apuestas_Boletos FOREIGN KEY (IDBoleto) REFERENCES Boletos (ID) ON UPDATE CASCADE ON DELETE CASCADE
@@ -103,3 +106,85 @@ CREATE TABLE Boletos_Partidos
 	CONSTRAINT FK_Boletos_Partidos_Boletos FOREIGN KEY (IDBoleto) REFERENCES Boletos (ID) ON UPDATE CASCADE ON DELETE NO ACTION,
 	CONSTRAINT FK_Boletos_Partidos_Partidos FOREIGN KEY (IDPartido) REFERENCES Partidos (ID) ON UPDATE NO ACTION ON DELETE NO ACTION --Cambiar estos dos update y cascade
 )
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Tipo CHECK (Tipo IN ('Simple', 'Multiple'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion1 CHECK (Prediccion1 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion2 CHECK (Prediccion2 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion3 CHECK (Prediccion3 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion4 CHECK (Prediccion4 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion5 CHECK (Prediccion5 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion6 CHECK (Prediccion6 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion7 CHECK (Prediccion7 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion8 CHECK (Prediccion8 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion9 CHECK (Prediccion9 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion10 CHECK (Prediccion10 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion11 CHECK (Prediccion11 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion12 CHECK (Prediccion12 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion13 CHECK (Prediccion13 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion14 CHECK (Prediccion14 IN ('1', 'X', '2', '1X', '12', 'X2', '1X2'))
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Prediccion15 CHECK (PrediccionPleno IN ('0', '1', '2', 'M', '01', '02', '0M', 
+																									'12', '1M', '2M', '012', '01M', '12M', '012M'))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud1 CHECK ((Tipo = 'Simple' AND LEN (Prediccion1) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion1) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud2 CHECK ((Tipo = 'Simple' AND LEN (Prediccion2) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion2) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud3 CHECK ((Tipo = 'Simple' AND LEN (Prediccion3) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion3) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud4 CHECK ((Tipo = 'Simple' AND LEN (Prediccion4) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion4) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud5 CHECK ((Tipo = 'Simple' AND LEN (Prediccion5) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion5) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud6 CHECK ((Tipo = 'Simple' AND LEN (Prediccion6) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion6) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud7 CHECK ((Tipo = 'Simple' AND LEN (Prediccion7) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion7) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud8 CHECK ((Tipo = 'Simple' AND LEN (Prediccion8) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion8) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud9 CHECK ((Tipo = 'Simple' AND LEN (Prediccion9) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion9) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud10 CHECK ((Tipo = 'Simple' AND LEN (Prediccion10) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion10) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud11 CHECK ((Tipo = 'Simple' AND LEN (Prediccion11) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion11) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud12 CHECK ((Tipo = 'Simple' AND LEN (Prediccion12) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion12) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud13 CHECK ((Tipo = 'Simple' AND LEN (Prediccion13) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion13) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_Longitud14 CHECK ((Tipo = 'Simple' AND LEN (Prediccion14) = 1) 
+																	OR (Tipo = 'Multiple' AND LEN (Prediccion14) IN (2, 3)))
+
+ALTER TABLE Apuestas ADD CONSTRAINT CK_Apuestas_LongitudPleno CHECK ((Tipo = 'Simple' AND LEN (PrediccionPleno) = 1)
+																	OR (Tipo = 'Multiple' AND LEN (PrediccionPleno) IN (2, 3, 4)))
+
+ALTER TABLE Boletos ADD CONSTRAINT CK_Boletos_NumeroApuesta CHECK (NumeroApuesta > 0 AND NumeroApuesta < 9)
+
+GO
+
+CREATE TRIGGER numeroApuestas ON Apuestas
+AFTER INSERT AS
+
+IF EXISTS (SELECT I.Tipo
+				FROM inserted AS I
+				INNER JOIN
+				Boletos AS B
+				ON I.IDBoleto = B.ID
+				WHERE B.NumeroApuestas > 1 AND I.Tipo = 'Multiple')
+BEGIN
+	ROLLBACK
+END
+
+GO
